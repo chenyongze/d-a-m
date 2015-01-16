22d0dccd1866cc9ea63ab08a29191c99963326e6
liyongjiang <liyongjiang@mofang.com>
2015-01-16 18:21:27 +0800

    修改更新脚本

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ chown -R www:www /data/www/db.admin.mofang.com
 chmod -R 755 /data/www/db.admin.mofang.com
 rsync -avzP --password-file=/etc/rsyncd.pwd /data/www/db.admin.mofang.com/* mofang@42.62.77.208::salt-rsync/db.admin.mofang.com
 
-curl -d "username=huxiaojun&password=hxj000000&groups=api&project=db.admin.mofang.com" $update 
+curl -d "username=update&password=mofang888&groups=api&project=db.admin.mofang.com" $update 

1c61a204c649548c6e7cf4e5472e2befae607e89
chenyongze <chenyongze@mofang.com>
2015-08-13 11:33:39 +0800

    支持字段组，单选

--- protected/views/cardItem/_form_item_normal_select.php
+++ protected/views/cardItem/_form_item_normal_select.php
@@ -1,7 +1,17 @@
 <!-- 单选字段 -->
+<!-- by author:yongze -->
 <?php 
-	$selectItems = $data['extra']['field_info']['select_value'];
-	$selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : -1;
+	if (!empty($group)) {
+	    $fieldLabelId = "singleChangeLable_{$group}_{$key}_";
+	    $selectItems = $data['extra']['field_info']['select_value'];
+	    $selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : -1;
+	    $fieldName = "CardItem[data][{$group}][{$key}][$enName]";
+	} else {
+	    $fieldLabelId = "singleChangeLable_";
+	    $selectItems = $data['extra']['field_info']['select_value'];
+	    $selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : -1;
+	    $fieldName = "CardItem[data][$enName]";
+	}
 ?>
 <div class="row label">
 	<div class="span2">
@@ -11,8 +21,8 @@
 <div class="row">
 	<?php foreach($selectItems as $key=>$value): ?>
 	<div class="span2">
-		<input id="singleChangeLable_<?php echo $enName.'_'.$key;?>" type="radio" <?php if ($selectItemValue==$value['value']) echo 'checked="checked"';?> name="CardItem[data][<?php echo $enName;?>]" value="<?=$value['value']?>" >
-		<label for="singleChangeLable_<?php echo $enName.'_'.$key;?>" style="display:inline;"><?php echo isset($value['value'])?$value['value']:$value;?></label>
+		<input id="<?php echo $fieldLabelId.$enName.'_'.$key;?>" type="radio" <?php if ($selectItemValue==$value['value']) echo 'checked="checked"';?> name="<?php echo $fieldName;?>" value="<?=$value['value']?>" >
+		<label for="<?php echo $fieldLabelId.$enName.'_'.$key;?>" style="display:inline;"><?php echo isset($value['value'])?$value['value']:$value;?></label>
 	</div>
 	<?php endforeach;?>
 </div>

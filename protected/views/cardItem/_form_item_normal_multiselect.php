9b1ffad07b725158302a06a64e5cd68fcf60b42d
chenyongze <chenyongze@mofang.com>
2015-08-13 11:48:33 +0800

    支持字段组 多选栏目

--- protected/views/cardItem/_form_item_normal_multiselect.php
+++ protected/views/cardItem/_form_item_normal_multiselect.php
@@ -1,8 +1,23 @@
 <!-- 多选字段 -->
-<?php 
-	$selectItems = $data['extra']['field_info']['select_value'];
-	$selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : array();
+<!-- by author:yongze -->
+<?php
+
+	if (!empty($group)) {
+	    $fieldLabelId = "multiChangeLable_{$group}_{$key}_";
+	    $selectItems = $data['extra']['field_info']['select_value'];
+	    $selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : array();
+	    $fieldName = "CardItem[data][{$group}][{$key}][$enName]";
+	} else {
+	    $fieldLabelId = "multiChangeLable_";
+	    $selectItems = $data['extra']['field_info']['select_value'];
+	    $selectItemValue = (isset($itemData[$enName])&&!empty($itemData[$enName])) ? $itemData[$enName] : array();
+	    $fieldName = "CardItem[data][$enName]";
+	}
+	
 ?>
+
+
+
 <div class="row label">
 	<div class="span2">
 		<?php echo $data['name']?>
@@ -11,8 +26,8 @@
 <div class="row">
 	<?php foreach($selectItems as $key=>$value): ?>
 	<div class="span2">
-		<input id="multiChangeLable_<?php echo $enName.'_'.$key;?>" type="checkbox" <?php if (in_array($value['value'], $selectItemValue)) echo 'checked="checked"';?> name="CardItem[data][<?php echo $enName;?>][]" value="<?=$value['value']?>" >
-		<label for="multiChangeLable_<?php echo $enName.'_'.$key;?>" style="display:inline;"><?php echo isset($value['value'])?$value['value']:$value;?></label>
+		<input id="<?php echo $fieldLabelId.$enName.'_'.$key;?>" type="checkbox" <?php if (in_array($value['value'], $selectItemValue)) echo 'checked="checked"';?> name="<?php echo $fieldName;?>[]" value="<?=$value['value']?>" >
+		<label for="<?php echo $fieldLabelId.$enName.'_'.$key;?>" style="display:inline;"><?php echo isset($value['value'])?$value['value']:$value;?></label>
 	</div>
 	<?php endforeach;?>
 </div>
